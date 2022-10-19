bbcfff809a498a35f8d1dda65c7956b3a5843f51
priyanshusingh92 <singhpriyanshu772@gmail.com>
2022-10-19 15:57:48 +0530

    test commit for changing git user account

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -2,7 +2,7 @@ import classes from './MealItem.module.css'
 
 const MealItem =props =>{
     const price  = `$${props.price.toFixed(2)}`
-    return <li>
+    return <li className={classes.meal}>
         <div>
             <h3>{props.name}</h3>
             <div className={classes.description}>{props.description}</div>
