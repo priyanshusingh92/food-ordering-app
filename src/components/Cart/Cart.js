8a9a17418ff1dec4c03167cb7ac4a745f310ddab
priyanshusingh92 <singhpriyanshu772@gmail.com>
2022-10-20 16:08:19 +0530

    cart and meal item

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,4 @@
+import classes from './Cart.module.css'
+const Cart = props=>{}
+
+export default Cart;
--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -1,7 +1,8 @@
 import classes from './MealItem.module.css'
+import MealItemForm from './MealItemForm';
 
-const MealItem =props =>{
-    const price  = `$${props.price.toFixed(2)}`
+const MealItem = props => {
+    const price = `$${props.price.toFixed(2)}`
     return <li className={classes.meal}>
         <div>
             <h3>{props.name}</h3>
@@ -9,7 +10,7 @@ const MealItem =props =>{
             <div className={classes.price}>{price}</div>
         </div>
         <div>
-
+            <MealItemForm id={props.id} />
         </div>
     </li>
 }
