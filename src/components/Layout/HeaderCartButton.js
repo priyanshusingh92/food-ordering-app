3a865d113fc0215b7c8eda93789f82e041a5c5fd
priyanshusingh92 <singhpriyanshu772@gmail.com>
2022-10-22 22:56:29 +0530

    cart item add remove functionality

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,23 @@
-import { useContext } from "react"
+import { useContext, useEffect,useState } from "react"
 import CartContext from "../../store/cart-context"
 import CartIcon from "../Cart/CartIcon"
 import classes from './HeaderCartButton.module.css'
 const HeaderCartButton = props=>{
+
+    const [btnIsHighlighted, setBtnIsHighlighted]= useState(false)
     const cartCtx = useContext(CartContext);
     const numberOfCartItems = cartCtx.items.reduce((num,item)=>{
         return num + item.amount ;
     },0);
+const btnClasses = `${classes.button} ${btnIsHighlighted  ? classes.bump: ''}`;
 
-    return<button onClick={props.onClick} className={classes.button}>
+useEffect(()=>{
+    if(cartCtx.items.length===0){
+        return;
+    }
+    setBtnIsHighlighted(true);
+},[cartCtx.items])
+    return<button onClick={props.onClick} className={btnClasses}>
         <span className={classes.icon}>
             <CartIcon/>
         </span>
